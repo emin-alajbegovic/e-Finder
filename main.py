b83ccdf350c1ff677abb6a1c87ec6ea487d7f55a
Emin Alajbegovic <emin.alajbegovic@iwgplc.com>
2022-10-22 17:48:09 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+
+print("Current working directory is: " + os.getcwd())
+print(list(os.listdir()))
+
+
+# folder path input
+print("Enter folder path")
+path = os.path.abspath(input())
+
+# for storing size of each
+# file
+size = 0
+
+# for storing the size of
+# the largest file
+max_size = 0
+
+# for storing the path to the
+# largest file
+max_file =""
+
+# walking through the entire folder,
+# including subdirectories
+
+
+for folder, subfolders, files in os.walk(path):
+	
+	# checking the size of each file
+	for file in files:
+		# size = len(os.stat(os.path.join( folder, file )))
+
+		# size = len(os.stat(os.path.join( folder, file )))
+        
+		size = len(' '.join(file.split()))
+
+		# updating maximum length
+		if size>max_size:
+			max_size = size
+			max_s = file
+			max_file = os.path.join( folder, file )
+
+print("The largest file is: " + max_file)
+print("The longest name is: " + max_s)
+print('Size: '+ str(max_size) + ' characters')
